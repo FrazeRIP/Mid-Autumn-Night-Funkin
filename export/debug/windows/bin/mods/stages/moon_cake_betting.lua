6e87944b45bf395b655067cd73e55083dbcbd498
Luckyfishi <320247251@qq.com>
2022-08-20 19:10:47 +0800

    博饼舞台+人物文件

---
+++ export/debug/windows/bin/mods/stages/moon_cake_betting.lua
@@ -0,0 +1,21 @@
+function onCreate()
+	makeLuaSprite('sky', 'stages/moon_cake_betting/PinkGhostLayer0', 0, 0);
+	setScrollFactor('sky', 0.9, 0.9);
+
+	makeLuaSprite('stage', 'stages/moon_cake_betting/PinkGhostLayer1', 0, 0);
+
+	makeLuaSprite('toast', 'stages/moon_cake_betting/PinkGhostLayer2', 1960, 125);
+
+	makeAnimatedLuaSprite('guest0','stages/moon_cake_betting/PinkGhostGuest/PinkGhostGuest', 296, 416);
+	addAnimationByPrefix('guest0','idle','backdrop',24,false);
+
+	addLuaSprite('sky', false);
+	addLuaSprite('stage', false);
+	addLuaSprite('toast', false);
+	addLuaSprite('guest0', false);
+end
+
+function onBeatHit()
+	-- triggered 4 times per section
+	objectPlayAnimation('guest0','idle',true);
+end
