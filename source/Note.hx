c9e86caba3ebed6d649264fa8669cd1acf8c322b
FrazeRIP <36977197+FrazeRIP@users.noreply.github.com>
2023-07-02 22:21:08 -0500

    把note和strumnote里所有关于像素的的东西注释掉了

--- source/Note.hx
+++ source/Note.hx
@@ -235,8 +235,8 @@ class Note extends FlxSprite
 
 			offsetX -= width / 2;
 
-			if (PlayState.isPixelStage)
-				offsetX += 30;
+			// if (PlayState.isPixelStage)
+			// 	offsetX += 30;
 
 			if (prevNote.isSustainNote)
 			{
@@ -258,18 +258,18 @@ class Note extends FlxSprite
 					prevNote.scale.y *= PlayState.instance.songSpeed;
 				}
 
-				if(PlayState.isPixelStage) {
-					prevNote.scale.y *= 1.19;
-					prevNote.scale.y *= (6 / height); //Auto adjust note size
-				}
+				// if(PlayState.isPixelStage) {
+				// 	prevNote.scale.y *= 1.19;
+				// 	prevNote.scale.y *= (6 / height); //Auto adjust note size
+				// }
 				prevNote.updateHitbox();
 				// prevNote.setGraphicSize();
 			}
 
-			if(PlayState.isPixelStage) {
-				scale.y *= PlayState.daPixelZoom;
-				updateHitbox();
-			}
+			// if(PlayState.isPixelStage) {
+			// 	scale.y *= PlayState.daPixelZoom;
+			// 	updateHitbox();
+			// }
 		} else if(!isSustainNote) {
 			earlyHitMult = 1;
 		}
@@ -302,40 +302,42 @@ class Note extends FlxSprite
 
 		var lastScaleY:Float = scale.y;
 		var blahblah:String = arraySkin.join('/');
-		if(PlayState.isPixelStage) {
-			if(isSustainNote) {
-				loadGraphic(Paths.image('pixelUI/' + blahblah + 'ENDS'));
-				width = width / 4;
-				height = height / 2;
-				originalHeightForCalcs = height;
-				loadGraphic(Paths.image('pixelUI/' + blahblah + 'ENDS'), true, Math.floor(width), Math.floor(height));
-			} else {
-				loadGraphic(Paths.image('pixelUI/' + blahblah));
-				width = width / 4;
-				height = height / 5;
-				loadGraphic(Paths.image('pixelUI/' + blahblah), true, Math.floor(width), Math.floor(height));
-			}
-			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
-			loadPixelNoteAnims();
-			antialiasing = false;
+		// if(PlayState.isPixelStage) {
+		// 	if(isSustainNote) {
+		// 		loadGraphic(Paths.image('pixelUI/' + blahblah + 'ENDS'));
+		// 		width = width / 4;
+		// 		height = height / 2;
+		// 		originalHeightForCalcs = height;
+		// 		loadGraphic(Paths.image('pixelUI/' + blahblah + 'ENDS'), true, Math.floor(width), Math.floor(height));
+		// 	} else {
+		// 		loadGraphic(Paths.image('pixelUI/' + blahblah));
+		// 		width = width / 4;
+		// 		height = height / 5;
+		// 		loadGraphic(Paths.image('pixelUI/' + blahblah), true, Math.floor(width), Math.floor(height));
+		// 	}
+		// 	setGraphicSize(Std.int(width * PlayState.daPixelZoom));
+		// 	loadPixelNoteAnims();
+		// 	antialiasing = false;
 
-			if(isSustainNote) {
-				offsetX += lastNoteOffsetXForPixelAutoAdjusting;
-				lastNoteOffsetXForPixelAutoAdjusting = (width - 7) * (PlayState.daPixelZoom / 2);
-				offsetX -= lastNoteOffsetXForPixelAutoAdjusting;
+		// 	if(isSustainNote) {
+		// 		offsetX += lastNoteOffsetXForPixelAutoAdjusting;
+		// 		lastNoteOffsetXForPixelAutoAdjusting = (width - 7) * (PlayState.daPixelZoom / 2);
+		// 		offsetX -= lastNoteOffsetXForPixelAutoAdjusting;
 
-				/*if(animName != null && !animName.endsWith('end'))
-				{
-					lastScaleY /= lastNoteScaleToo;
-					lastNoteScaleToo = (6 / height);
-					lastScaleY *= lastNoteScaleToo;
-				}*/
-			}
-		} else {
+		// 		/*if(animName != null && !animName.endsWith('end'))
+		// 		{
+		// 			lastScaleY /= lastNoteScaleToo;
+		// 			lastNoteScaleToo = (6 / height);
+		// 			lastScaleY *= lastNoteScaleToo;
+		// 		}*/
+		// 	}
+		// } else {
 			frames = Paths.getSparrowAtlas(blahblah);
 			loadNoteAnims();
 			antialiasing = ClientPrefs.globalAntialiasing;
-		}
+		//}
+
+
 		if(isSustainNote) {
 			scale.y = lastScaleY;
 		}
