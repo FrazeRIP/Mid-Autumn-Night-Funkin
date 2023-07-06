8fc61735ab00ab928ba1e31d7f316069ca04a606
FrazeRIP <36977197+FrazeRIP@users.noreply.github.com>
2023-07-05 23:17:33 -0500

    pinkghost新贴图和stage

--- example_mods/stages/qi_xi_ting.lua
+++ example_mods/stages/qi_xi_ting.lua
@@ -4,20 +4,30 @@ function onCreate()
 
 	makeLuaSprite('stage', 'stages/moon_cake_betting/PinkGhostLayer1', 0, 0);
 
-	makeLuaSprite('toast', 'stages/moon_cake_betting/PinkGhostLayer2', 1960, 125);
+	makeLuaSprite('cover', 'stages/moon_cake_betting/PinkGhostLayer2', 0, 0);
 
-	makeAnimatedLuaSprite('guest0','stages/moon_cake_betting/PinkGhostGuest/PinkGhostGuest', 296, 416);
-	addAnimationByPrefix('guest0','idle','backdrop',24,false);
+	makeAnimatedLuaSprite('guest0','stages/moon_cake_betting/PinkGhostGuest/PinkGhostGuest2', 200, 370);
+	addAnimationByPrefix('guest0','idle','backdrop6',24,false);
+	makeAnimatedLuaSprite('guest1','stages/moon_cake_betting/PinkGhostGuest/PinkGhostGuest3', 2100, 416);
+	addAnimationByPrefix('guest1','idle','backdrop7',24,false);
+	
+	makeAnimatedLuaSprite('guestFront','stages/moon_cake_betting/PinkGhostGuest/PinkGhostGuest1', -140, 900);
+	addAnimationByPrefix('guestFront','idle','backdrop5',24,false);
+	setScrollFactor('guestFront', .8, .8);
 
 	addLuaSprite('sky', false);
 	addLuaSprite('stage', false);
-	addLuaSprite('toast', false);
+	addLuaSprite('cover', false);
 	addLuaSprite('guest0', false);
+	addLuaSprite('guest1', false);
+	addLuaSprite('guestFront', true);
 end
 
 function onBeatHit()
 	-- triggered 4 times per section
 	objectPlayAnimation('guest0','idle',true);
+	objectPlayAnimation('guest1','idle',true);
+	objectPlayAnimation('guestFront','idle',true);
 end
 
 
