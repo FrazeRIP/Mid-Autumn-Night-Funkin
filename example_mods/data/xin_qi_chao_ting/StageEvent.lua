23d41732b70cf6dcab86133d7e3a4dd71567fa93
FrazeRIP <fanxiao805@gmail.com>
2023-07-25 01:37:15 -0500

    删了没有用上的原版素材 保留了周1的素材

---
+++ example_mods/data/xin_qi_chao_ting/StageEvent.lua
@@ -0,0 +1,13 @@
+
+function onCreatePost( ... )
+	doTweenX('paolaSX', 'dad.scale', .9, .00001)
+	doTweenY('paolaSY', 'dad.scale', .9, .00001)
+	doTweenX('bfSX', 'boyfriend.scale', .6, .00001)
+	doTweenY('bfSY', 'boyfriend.scale', .6,.00001)
+	doTweenX('gfSX', 'gf.scale', .7, .00001)
+	doTweenY('gfSY', 'gf.scale', .7, .00001)
+	
+	doTweenY('bfPY', 'boyfriend',300, .00001)
+
+	setProperty('gf.visible',false)
+end
