9dee6db59aae5f3d94e21046b28a4ddbc0c17d7c
LunarSurface <wonder_bernkastel@qq.com>
2023-08-31 16:23:41 +0800

    MainState大幅更新、StoryState基础更新，灯谜机制更新完毕

--- source/PuzzleMember.hx
+++ source/PuzzleMember.hx
@@ -1,5 +1,9 @@
 package;
 
+import haxe.Timer;
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxEase.EaseFunction;
 import flixel.text.FlxText;
 import flixel.FlxBasic;
 import flixel.FlxCamera;
@@ -11,6 +15,8 @@ import flixel.text.FlxText;
 import flixel.input.mouse.FlxMouseEventManager;
 import flixel.input.mouse.FlxMouse;
 import flixel.group.FlxSpriteGroup;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
 
 
 class PuzzleMember extends FlxSpriteGroup
@@ -18,19 +24,22 @@ class PuzzleMember extends FlxSpriteGroup
     public var memberText:FlxText;
     public var isAnswer:Bool;
     public var bgSprite:FlxSprite;
-    
+
     public override function new(word:String,x:Float,y:Float) {
         super();
 
-        this.memberText=new FlxText(0,0,0,word,24);
-        this.width=30;
-        this.height=30;
+        // FlxG.plugins.add(new FlxMouseEventManager());
+
+        this.memberText=new FlxText(5,0,0,word,34);
+        this.width=50;
+        this.height=50;
         this.x=x;
         this.y=y;
         memberText.setFormat("assets/fonts/ZhengDaoCuShuTi.ttf",memberText.size);
-        memberText.color=0x000000;
+        memberText.color=0xFF000000;
 
-        bgSprite=new FlxSprite(0,0).makeGraphic(30,30);
+        bgSprite=new FlxSprite(0,0).makeGraphic(50,50,0xFFFFFFFF);
+        bgSprite.alpha=0.1;
 
         add(bgSprite);
         add(memberText);
@@ -44,7 +53,11 @@ class PuzzleMember extends FlxSpriteGroup
     
             // if (sprTracker != null)
             //     memberText.setPosition(0,0);
-
+            if(PuzzleSubState.seletedAnswer)
+            {
+                memberText.color=0xff000000;
+                stopEvnet();
+            }
             
         }
     public function stopEvnet()
@@ -57,14 +70,45 @@ class PuzzleMember extends FlxSpriteGroup
     }
     function onMouseDown(_)
     {
-
+        var ROWToken:FlxSprite=new FlxSprite();
+        ROWToken.frames=Paths.getSparrowAtlas('storystate/puzzle/OX','mid-autumn');
+        ROWToken.animation.addByPrefix("right",'OX O0000',24,false);
+        ROWToken.animation.addByPrefix("wrong",'OX X0000',24,false);
+        if(isAnswer)
+        {
+            ROWToken.animation.play("right");
+            add(ROWToken);
+            FlxG.sound.play(Paths.sound('storystate/correct','mid-autumn'));
+            PuzzleSubState.seletedAnswer=true;
+            new FlxTimer().start(1.0,function(tmr:FlxTimer){ 
+                FlxTween.tween(ROWToken,{alpha:0},1,{onComplete:function(twn:FlxTween) {
+                remove(ROWToken);
+                }});});
+        }
+        else 
+        {
+            ROWToken.animation.play("wrong");
+            add(ROWToken);
+            FlxG.sound.play(Paths.sound('storystate/error','mid-autumn'));
+            new FlxTimer().start(1.0,function(tmr:FlxTimer){ 
+            FlxTween.tween(ROWToken,{alpha:0},1,{onComplete:function(twn:FlxTween) {
+            remove(ROWToken);
+            }});});
+        }
     }
+
     function onMouseOver(_)
     {
-        
+        memberText.color=0xFFBA7F48;
     }
+
     function onMouseOut(_)
     {
-
+        memberText.color=0xFF000000;
+    }
+    override function destroy() {
+        PuzzleSubState.seletedAnswer=false;
+        startEvnet();
+        super.destroy();
     }
 }
