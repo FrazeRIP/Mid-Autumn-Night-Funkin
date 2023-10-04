8880d929febaa07c64db3d5cdd323baa6bbdc0c4
Luckyfishi <320247251@qq.com>
2023-10-04 22:51:43 +0800

    增加萨拉舞台
    去除多余博饼舞台配置

---
+++ example_mods/stages/yu_long_mei.lua
@@ -0,0 +1,39 @@
+
+local xOffset = 0
+local yOffset = 0
+local scale = 1
+
+local secPerBeat
+
+function onCreate()
+	makeLuaSprite('sky', 'stages/yu_long_mei/SarahLayer4',xOffset-50, yOffset+0);
+	setScrollFactor('sky', 0.75, 0.75);
+	scaleObject('sky', scale, scale);
+
+	makeLuaSprite('floor', 'stages/yu_long_mei/SarahLayer3', xOffset+ 0, yOffset+0);
+	scaleObject('floor', scale, scale);
+
+	makeLuaSprite('light', 'stages/yu_long_mei/SarahLayer2', xOffset+ 0, yOffset+0);
+	scaleObject('light', scale, scale);
+
+	makeLuaSprite('candle', 'stages/yu_long_mei/SarahLayer1', xOffset+ 0, yOffset+0);
+	scaleObject('candle', scale, scale);
+
+
+	addLuaSprite('sky', false);	
+	addLuaSprite('floor', false);
+	addLuaSprite('light', false);
+	addLuaSprite('candle', false);
+	
+end
+ 
+function onBeatHit()
+	
+end
+
+
+function onCreatePost( ... )
+	secPerBeat = 60/curBpm
+end
+
+
