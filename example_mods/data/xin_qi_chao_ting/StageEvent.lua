4711ea064e861fa7e7dbc9a8e828a58ffe5689b4
FrazeRIP <36977197+FrazeRIP@users.noreply.github.com>
2023-10-09 22:10:19 -0500

    新Nico，萨拉场景修改和箭头

--- example_mods/data/xin_qi_chao_ting/StageEvent.lua
+++ example_mods/data/xin_qi_chao_ting/StageEvent.lua
@@ -1,7 +1,7 @@
 
 function onCreatePost( ... )
-	doTweenX('paolaSX', 'dad.scale', .9, .00001)
-	doTweenY('paolaSY', 'dad.scale', .9, .00001)
+	doTweenX('paolaSX', 'dad.scale', .85, .00001)
+	doTweenY('paolaSY', 'dad.scale', .85, .00001)
 	doTweenX('bfSX', 'boyfriend.scale', .6, .00001)
 	doTweenY('bfSY', 'boyfriend.scale', .6,.00001)
 	doTweenX('gfSX', 'gf.scale', .7, .00001)
